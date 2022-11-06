f1eb60ea389baaac92dab075232e97d8eba2ba6a
sharetown <2267949319@qq.com>
2022-11-06 14:04:26 +0800

    lightly在线云编译器好像不支持这种相对路径导入

---
+++ 02_package/icomefromchina/name2.go
@@ -0,0 +1,4 @@
+package icomefromchina
+
+// MyName will be exported because it starts with a capital letter.
+var BearName = "Pooh"
---
+++ 02_package/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "main/stringutil"
+    "main/icomefromchina"
+
+)
+
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(shenzhen.BearName)
+
+}
