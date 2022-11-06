3f51b398c7cc5bff0a569b64478a751c1a854c31
sharetown <2267949319@qq.com>
2022-11-06 22:20:57 +0800

    补充当前包下的程序的执行形式

--- 04_scope/05_same-package/main.go
+++ 04_scope/05_same-package/main.go
@@ -6,9 +6,9 @@ func main() {
 	fmt.Println(x)
 }
 
-//重要的
-//要运行此代码：
-// go run *.go
-// ---- OR ----
-// go build
-//./05_名称-包装
+/*
+如果要执行该文件,直接在编译器中执行该main文件会报错
+需要到05_same_package目录下执行go build .\05_same_package
+此时会得到一个名为05_same_package.exe的只执行文件
+直接执行该可执行文件才可以正常运行
+*/
