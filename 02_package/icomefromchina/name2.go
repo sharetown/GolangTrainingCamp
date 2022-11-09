ddd53833bd0c5e5cf8556d6be762dc4941c8ab97
sharetown <2267949319@qq.com>
2022-11-09 09:19:26 +0800

    替换内容为go by example

--- 02_package/icomefromchina/name2.go
+++
@@ -1,4 +0,0 @@
-package shenzhen
-
-// MyName will be exported because it starts with a capital letter.
-var BearName = "Pooh"
--- 04_scope/01_package-scope/02_visibility/vis/name.go
+++
@@ -1,6 +0,0 @@
-package vis
-
-// MyName可以被导出，因为它以大写字母开头
-var MyName = "Todd"
-//但是yourName不可以被导出，不能在其他包中访问到当前资源，只能在本包下访问
-var yourName = "Future Rock Star Programmer"
